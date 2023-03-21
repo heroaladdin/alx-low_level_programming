edda4fae7200cf14be5f243eb3e0ff430f02f329
heroaladdin <alaaeddinennajari@gmail.com>
2023-03-20 19:29:08 -0700

    advance

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
 
 /**
-* main - print the size of various types
-* Return: 0 if exited properly, non-zero otherwise
-*/
+ * main - print the size of various types
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+
 int main(void)
 {
         printf("Size of a char: %d byte(s)\n", sizeof(char));
